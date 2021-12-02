afe851c6c1359458af1de70744d096804e55e857
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-12-02 18:49:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ lwy=0;
 //right wrist
 rwx=0;
 rwy=0;
+// is playing
+song1_status=0;
+song1_status=song1.isPlaying();
+song2_status=0;
+song2_status=song2.isPlaying();
 function preload(){
    song1=loadSound("music.mp3")
    song2=loadSound("music2.mp3")
@@ -21,9 +26,6 @@ function setup(){
 }
 function draw(){
     image(video,0,0,600,500);
-    fill("#FF0000");
-    stroke("#FF0000");
-    circle(lwx,lwy,20);
 }
 function modelLoaded() {
   console.log('PoseNet has come by taxi');
@@ -38,5 +40,20 @@ function gotPoses(results){
 	lwx = results[0].pose.leftWrist.x;
 	lwy = results[0].pose.leftWrist.y;
 	console.log("leftWristX = " + lwx +" leftWristY = "+ lwy);
+	    
+	//scores
+	scoreRightWrist =  results[0].pose.keypoints[10].score;
+	scoreLeftWrist =  results[0].pose.keypoints[9].score;
+    }
+    if(scoreLeftWrist > 0.2){
+    	fill("#FF0000");
+    	stroke("#FF0000");
+    	circle(lwx,lwy,20);
+	song2.stop();
+	if song1_status == false{
+	   song1.play();
+	   document.getElementById("song-name").innerhtml == "Song 1"
+	}
+	 
     }
 }
