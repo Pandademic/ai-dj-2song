7ed1980a5f56b9f491647b0953a4cea2c65df0e6
Atha <74566464+Pandademic@users.noreply.github.com>
2022-04-13 15:43:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,14 +4,17 @@ song2="";
 //left wrist 
 lwx=0;
 lwy=0;
+lws = 0;
 //right wrist
 rwx=0;
 rwy=0;
+rws=0;
 // is playing
 song1_status=0;
 song1_status=song1.isPlaying();
 song2_status=0;
 song2_status=song2.isPlaying();
+
 function preload(){
    song1=loadSound("music.mp3")
    song2=loadSound("music2.mp3")
@@ -35,11 +38,11 @@ function gotPoses(results){
         
 	rwx = results[0].pose.rightWrist.x;
     	rwy = results[0].pose.rightWrist.y;
-	console.log("rightWristX = " + rwx +" rightWristY = "+ rwy);
+	console.log("%crightWristX = " + rwx +" rightWristY = "+ rwy,"font-size: 10px; margin: 30px 0; padding: 5px 20px; color: #fff; background: linear-gradient(0deg, rgba(111,52,180,10) 0%, rgba(223,19,29,11) 50%, rgba(252,176,69,1) 100%);");
 
 	lwx = results[0].pose.leftWrist.x;
 	lwy = results[0].pose.leftWrist.y;
-	console.log("leftWristX = " + lwx +" leftWristY = "+ lwy);
+	console.log("%cleftWristX = " + lwx +" leftWristY = "+ lwy,"font-size: 10px; margin: 30px 0; padding: 5px 20px; color: #fff; background: linear-gradient(0deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);");
 	    
 	//scores
 	scoreRightWrist =  results[0].pose.keypoints[10].score;
